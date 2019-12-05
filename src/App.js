0a3ed14d96cf62a5c9e7c345039678340ced6eef
Damian <daho2353@colorado.edu>
2019-12-05 09:26:32 -0700

    Update App.js
    Static version of the app component (no timer or card component yet)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function resetBoard()
+{
+  //Resets the state and props to their initial state 
+}
+
 function App() {
   return (
     <div className="App">
-
+      <h3> 0:00 </h3> {/*timer component goes here*/}
+      <table>
+        <tr>
+          <td> test </td> {/*card component goes in <td> slots*/}
+          <td> test </td>
+          <td> test </td>
+          <td> test </td>
+          <td> test </td>
+        </tr>
+        <tr>
+          <td> test </td>
+          <td> test </td>
+          <td> test </td>
+          <td> test </td>
+          <td> test </td>
+        </tr>
+      </table>
+      <button onClick={() => resetBoard(/*put setState hook here*/)}> Reset </button> 
     </div>
   );
 }
