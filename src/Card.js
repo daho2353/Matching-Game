2bcce141152eedaa184fda52dfc3ab226ee54b7f
Damian <daho2353@colorado.edu>
2019-12-05 10:22:05 -0700

    loose ends

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 function Card() {
 	return (
-<<<<<<< HEAD
-		<td style ={{backgroundColor: 'black'}}></td>
-=======
+
 		<td style ={{backgroundColor: 'black', border: 'solid 1px white' }}></td>
->>>>>>> b5b892879a743087e6c159891f42147c889ade45
-	); 
+
+	);
 }
 
 export default Card; 
