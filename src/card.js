1480b9d5bb25b2e8c8cf1bbf0973ce2209fd5916
Damian <daho2353@colorado.edu>
2019-12-05 10:21:00 -0700

    Update card.js

--- src/card.js
+++ src/card.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Card() {
 	return (
+<<<<<<< HEAD
 		<td style ={{backgroundColor: 'black'}}></td>
+=======
+		<td style ={{backgroundColor: 'black', border: 'solid 1px white' }}></td>
+>>>>>>> b5b892879a743087e6c159891f42147c889ade45
 	); 
 }
 
