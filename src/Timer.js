d678527a2e7d726de5f0085593e3fc4a95014bee
isqu6491 <31741165+isqu6491@users.noreply.github.com>
2019-12-10 10:48:32 -0700

    added to timer.js

--- src/Timer.js
+++ src/Timer.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 
 function Timer (props){
+const [currentTime, setCurrentTime] = useState(0);
+const [isActive, setIsActive] = useState(false);
 
-	return(
-		<h3> 0:00 </h3>
-	);
+	if (props.startTimer == true){
+   	setIsActive = true;
+   }
+
+   if (props.stopTimer == true){
+
+   	setIsActive = false;
+   }
+
+  
+
+
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setCurrentTime(currentTime => currentTime + 1);
+      }, 1000);
+    } else if (!isActive && currentTime !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, currentTime]);
+
+
+
+  return (
+    <div>
+      <div> {currentTime}s </div>
+    </div>
+  )
+	
+
 }
 
 export default Timer; 
