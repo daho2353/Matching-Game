fd7b07b9c0b1191baafd2eb421d5dc71f659cbfb
feve1752 <feve1752@colorado.edu>
2019-12-10 09:50:23 -0700

    switch case for card.js

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,38 @@
 import React from 'react'; 
 
-function Card() {
+
+function Card(props) {	
+	var cardColor; 
+
+	switch() {
+		case 0:
+			cardColor = 'red' ;
+    		break;
+    	case 1:
+    		cardColor = 'yellow'; 
+    		break;
+    	case 2: 
+    		cardColor = 'blue';
+    		break; 
+    	case 3: 
+    		cardColor = 'green'; 
+    		break; 
+    	case 4: 
+    		cardColor = 'gray'; 
+    		break; 
+    	case 5: 
+    		cardColor = 'white';
+    		break; 
+    	case 6: 
+    		cardColor = 'black';
+    		break; 
+	}
+
 	return (
 
-		<td style ={{backgroundColor: 'black', border: 'solid 1px white' }}></td>
+		<td style ={{backgroundColor: cardColor, border: 'solid 1px white' }}></td>
 
-	);
+		);
 }
 
 export default Card; 
