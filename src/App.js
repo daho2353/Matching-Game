d9306bd46a666b2774373bed90b28e0d6c552da1
Damian <daho2353@colorado.edu>
2019-12-11 20:51:46 -0700

    Updating App.js (tentative)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 import Timer from'./Timer';
 import './App.css';
+import { useState } from 'react';
+
+const possibleIDs = [1,1,2,2,3,3,4,4,5,5];
 
 function resetBoard()
 {
@@ -9,12 +12,35 @@ function resetBoard()
 }
 
 function App() {
+  const[cardState, setCardState] = useState(0); //0 unflipped, 1 flipped, 2 matched
+  const[victory, setVictory] = useState(0); //0 no victory, 1 victory
+  const[cardID, setCardID] = useState(0); //0 no ID 1 red 2 yellow 3 green 4 blue 5 purple
+  const[timer, setTimerState] = useState(0); //0 timer off, 1 timer on
+  function assignCard(cardIndex)
+  {
+    if(victory === 0 && cardID === 0)
+    {
+      var currentIndex = possibleIDs.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex)
+      {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = possibleIDs[currentIndex];
+        possibleIDs[currentIndex] = possibleIDs[randomIndex];
+        possibleIDs[randomIndex] = temporaryValue;
+      }
+    }
+    else if(victory === 0 && cardID !== 0)
+    {
+      
+    }
+  }
   return (
     <div className="App">
       <Timer/> 
       <table>
         <tr>
-          <Card/> 
+          <Card onClick={() => assignCard(cardState, setCardState, cardID, setCardID)}/> 
           <Card/>
           <Card/>
           <Card/>
@@ -28,7 +54,7 @@ function App() {
           <Card/> 
         </tr>
       </table>
-      <button onClick={() => resetBoard(/*put setState hook here*/)}> Reset </button> 
+      <button onClick={() => resetBoard(cardState, setCardState, cardID, setCardID, victory, setVictory)}> Reset </button> 
     </div>
   );
 }
